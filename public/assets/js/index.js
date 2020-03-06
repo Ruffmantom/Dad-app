1922774abb0e750d0b078fdc02d931174211ce8c
Tom Ruff <ruffmantom@gmail.com>
2020-03-06 11:01:47 -0700

    started on jquery logic for sign in and sign up html

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,25 @@
 // all of the Jquery and JS for the html in here
+const signUpForm = $("#sign-up-form");
+const signInForm = $("#sign-in-form");
+// buttons
+const logIn = $("#sign-in");
+const signUp = $("#sign-up-btn");
+// inputs
+const name = $("#name");
+const userName = $("#username");
+const password = $("#password");
+const loginUsername = $("#login-username");
+const loginPassword = $("#login-password");
+
 
 //  click events
-
+// sign up form
+signUpForm.submit(function (event) {
+    event.preventDefault();
+});
+// sign in form
+signInForm.submit(function (event) {
+    event.preventDefault();
+});
 // API calls
-console.log("HELLOOOO");
+console.log("HELLOOOO"); 
