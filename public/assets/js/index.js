666a084fd341476dcc9931387cf034d560a84dbe
johnathan olivas <jj.galvan13@yahoo.com>
2020-03-06 21:23:19 -0700

    trying to toggle image

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,11 @@
 // all of the Jquery and JS for the html in here
+
+function changeImage() {
+
+    if (document.getElementById("imgClickAndChange").src == "/public/images/icons/blackIcons/like.png") {
+        document.getElementById("imgClickAndChange").src = "/public/images/icons/blackIcons/liked.png"
+    }
+    else {
+        document.getElementById("imgClickAndChange").src = "/public/images/icons/blackIcons/like.png";
+    }
+}
