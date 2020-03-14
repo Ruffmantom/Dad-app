dc293fe54bc54a825d4b2a68c4c0da4825851f30
Tom Ruff <ruffmantom@gmail.com>
2020-03-14 12:53:04 -0600

    fixed all this

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "use_env_variable": "JAWS_DB",
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
 }
